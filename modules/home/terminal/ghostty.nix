5add5b664f49780f0eeb964119c67b261659a3d5
k1tyoodev <softdev.au@outlook.com>
2026-08-13 12:14:51 +0800

    feat(ghostty): use custom macOS app icon
    Ship a custom ghostty.icns in the dots tree and wire it through
    home-manager so Dock/app-switcher pick it up after rebuild.

--- modules/home/terminal/ghostty.nix
+++ modules/home/terminal/ghostty.nix
@@ -2,6 +2,7 @@
 
 {
   home.file.".config/ghostty/config".source = ../../../config/ghostty/config;
+  home.file.".config/ghostty/ghostty.icns".source = ../../../config/ghostty/ghostty.icns;
   home.file.".config/ghostty/themes" = {
     source = ../../../config/ghostty/themes;
     recursive = true;
